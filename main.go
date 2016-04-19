85e79295c65a98fe4d1ba8e1ebdcccd4c43e1eb0
Rekby <timofey@koolin.ru>
2016-04-19 14:11:28 +0300

    Filter by ip networks

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var (
 	CheckDomainTimeoutMS = flag.Int("timeout", 1000, "Timeout for every one check in milliseconds")
 	Parallel             = flag.Int("parallel", 10, "Parallel check count")
 	HTTPSPort            = flag.String("port", "443", "What port check")
+	IPNetworksFileName   = flag.String("networks", "", "Path to file with allowable ip/networks for connect to check domain. One network/subnet or ip address per line. Can use #-styled comments. Allow all by default.")
 )
 
 var (
@@ -26,13 +27,21 @@ var (
 )
 
 type Task struct {
-	Domain string
-	Result string
+	Domain        string
+	AllowNetworks *[]net.IPNet
+	Result        string
 }
+
+var AllowedNetworks *[]net.IPNet
 
 func main() {
 	flag.Usage = usage
 	flag.Parse()
+
+	// Parse allowable ip mask
+	if *IPNetworksFileName != "" {
+		AllowedNetworks = parseNetworksFile(*IPNetworksFileName)
+	}
 
 	CheckDomainTimeout = time.Millisecond * time.Duration(*CheckDomainTimeoutMS)
 
@@ -94,18 +103,42 @@ func checkDomain(task *Task) {
 	var res []string
 
 	defer func() {
-		task.Result = strings.Join(res, "\n")
+		task.Result = strings.Join(res, "; ")
 	}()
 
-	deadLine := time.Now().Add(CheckDomainTimeout)
 	ips, err := net.LookupIP(task.Domain)
 	if err != nil {
-		task.Result = "Error while domain lookup: " + err.Error()
+		res = append(res, "Error while domain lookup: "+err.Error())
 		return
 	}
+	if len(ips) == 0 {
+		res = append(res, "No ip addresses for the domain")
+		return
+	}
+
 	for _, ip := range ips {
+		if AllowedNetworks != nil {
+			ipAllowed := false
+			for _, network := range *AllowedNetworks {
+				ipString := ip.String()
+				ipnetString := network.String()
+				if ipString == ipnetString {
+
+				}
+				if network.Contains(ip) {
+					ipAllowed = true
+					break
+				}
+			}
+			if !ipAllowed {
+				res = append(res, "IP address denied by network filters: "+ip.String())
+				continue
+			}
+		}
+
 		func() {
-			conn, err := net.DialTimeout("tcp", ip.String()+":"+*HTTPSPort, deadLine.Sub(time.Now()))
+			deadLine := time.Now().Add(CheckDomainTimeout)
+			conn, err := net.DialTimeout("tcp", ip.String()+":"+*HTTPSPort, CheckDomainTimeout)
 			if err != nil {
 				res = append(res, fmt.Sprintf("Error while connect to IP: %v (%v)", ip, err))
 				return
@@ -113,14 +146,16 @@ func checkDomain(task *Task) {
 			defer conn.Close()
 
 			tlsConn := tls.Client(conn, &tls.Config{ServerName: task.Domain})
+			tlsConn.SetDeadline(deadLine)
 			err = tlsConn.Handshake()
 			if err != nil {
-				res = append(res, fmt.Sprintf("Error while handshake to IP '%v': %v", ip, err))
+				errString := err.Error()
+				res = append(res, fmt.Sprintf("Error while handshake to IP '%v': %v", ip, errString))
 				return
 			}
+			res = append(res, "OK: "+ip.String())
 		}()
 	}
-	res = append(res, "OK")
 }
 
 func filesReader(tasks chan<- Task, files ...string) {
@@ -144,6 +179,57 @@ func filesReader(tasks chan<- Task, files ...string) {
 			}
 		}()
 	}
+}
+
+func parseNetworksFile(fname string) *[]net.IPNet {
+	var res = []net.IPNet{}
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatalf("Can't open ip filters file '%v': %v\n", fname, err)
+	}
+	scanner := bufio.NewScanner(f)
+	lineNum := 0
+	for scanner.Scan() {
+		lineNum++
+		line := scanner.Text()
+		// Cut comment
+		if strings.Index(line, "#") != -1 {
+			line = line[:strings.Index(line, "#")]
+		}
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			continue
+		}
+
+		if strings.Index(line, "/") != -1 {
+			// CIDR
+			_, ipnet, err := net.ParseCIDR(line)
+			if err != nil {
+				log.Printf("Error while parse filters file '%v' line %v (%v):%v (CIDR)\n", fname, lineNum, line, err)
+				continue
+			}
+			res = append(res, *ipnet)
+		} else {
+			ip := net.ParseIP(line)
+			switch {
+
+			case ip == nil:
+				log.Printf("Error while parse filters file '%v' line %v (%v). (IP)\n", fname, lineNum, line)
+				continue
+			case ip.To4() != nil:
+				res = append(res, net.IPNet{IP: ip, Mask: net.CIDRMask(8*net.IPv4len, 8*net.IPv4len)})
+			case len(ip) == net.IPv6len:
+				res = append(res, net.IPNet{IP: ip, Mask: net.CIDRMask(8*net.IPv6len, 8*net.IPv6len)})
+			default:
+				log.Printf("Undetected ip address type: %v\n", line)
+			}
+		}
+	}
+	if scanner.Err() != nil {
+		log.Fatal("Error while read fileter file '%v': %v\n", fname, scanner.Err())
+	}
+	return &res
 }
 
 func stdinReader(tasks chan<- Task) {
